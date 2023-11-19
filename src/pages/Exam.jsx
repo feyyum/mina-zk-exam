2aa0ad77c8e8a417ab7d7f8db68ab3586b7dbf13
Feyyaz Numan Cavlak <52748261+feyyum@users.noreply.github.com>
2023-11-19 07:14:36 +0300

    Update Exam.jsx

--- src/pages/Exam.jsx
+++ src/pages/Exam.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 
-import Back from "../assets/Back.svg";
+import Back from "../assets/back.svg";
 
 function Exam() {
   const { examId } = useParams();
